dd53ec65d5428af58ec85698eaf7ef1fa4cede42
RomanFisc <romanfisc@gmail.com>
2024-07-23 15:01:30 -0400

    endpoints are working, able to create, get, delete tasks

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
 
-// Create a new task
 router.post('/tasks', taskController.createTask);
 
-// Get all tasks
 router.get('/tasks', taskController.getTasks);
 
-// Delete a task by ID
 router.delete('/tasks/:id', taskController.deleteTask);
 
 module.exports = router;
+
+
+
