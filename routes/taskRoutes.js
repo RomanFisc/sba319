6ea07c388f217826f59671ca5a8fa5df3a55400b
RomanFisc <romanfisc@gmail.com>
2024-07-25 10:42:50 -0400

    adding routes for the new collections

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const taskController = require('../controllers/taskController');
 
 router.post('/tasks', taskController.createTask);
-
 router.get('/tasks', taskController.getTasks);
-
 router.delete('/tasks/:id', taskController.deleteTask);
+
+router.get('/users', taskController.getAllUsers);
+router.post('/users', taskController.createUser);
+router.delete('/users/:id', taskController.deleteUser);
+
+router.get('/categories', taskController.getAllCategories);
+router.post('/categories', taskController.createCategory);
+router.delete('/categories/:id', taskController.deleteCategory);
 
 module.exports = router;
 
