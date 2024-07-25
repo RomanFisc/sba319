52e23dca5c5eb88a52be111664ae72b7b8151a88
RomanFisc <romanfisc@gmail.com>
2024-07-25 10:55:06 -0400

    renaming model page and creating individual controller files

---
+++ controllers/categoryController.js
@@ -0,0 +1,35 @@
+const Category = require('../models/category');
+
+// Controller to get all categories
+exports.getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Controller to create a new category
+exports.createCategory = async (req, res) => {
+  const category = new Category({
+    name: req.body.name
+  });
+  try {
+    const newCategory = await category.save();
+    res.status(201).json(newCategory);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Controller to delete a category
+exports.deleteCategory = async (req, res) => {
+  try {
+    const category = await Category.findByIdAndDelete(req.params.id);
+    if (!category) return res.status(404).json({ message: 'Category not found' });
+    res.status(200).json({ message: 'Category deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,110 +1,36 @@
 const Task = require('../models/task');
-const User = require('../models/user');
-const Category = require('../models/category');
 
-exports.createTask = async (req, res) => {
-  try {
-    const newTask = new Task({
-      title: req.body.title,
-      completed: req.body.completed
-    });
-    const savedTask = await newTask.save();
-    res.status(201).json(savedTask);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
+// Controller to get all tasks
 exports.getTasks = async (req, res) => {
   try {
     const tasks = await Task.find();
     res.status(200).json(tasks);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
+// Controller to create a new task
+exports.createTask = async (req, res) => {
+  const task = new Task({
+    title: req.body.title,
+    completed: req.body.completed
+  });
+  try {
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Controller to delete a task
 exports.deleteTask = async (req, res) => {
   try {
     const task = await Task.findByIdAndDelete(req.params.id);
     if (!task) return res.status(404).json({ message: 'Task not found' });
-    res.status(200).json({ message: 'Task deleted successfully' });
+    res.status(200).json({ message: 'Task deleted' });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ message: err.message });
   }
-};
-
-const getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-const createUser = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const newUser = new User({ username, email, password });
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-const getAllCategories = async (req, res) => {
-  try {
-    const categories = await Category.find();
-    res.status(200).json(categories);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-const createCategory = async (req, res) => {
-  try {
-    const { name } = req.body;
-    const newCategory = new Category({ name });
-    await newCategory.save();
-    res.status(201).json(newCategory);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-const deleteUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await User.findByIdAndDelete(id);
-    if (!result) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.status(200).json({ message: 'User deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-const deleteCategory = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await Category.findByIdAndDelete(id);
-    if (!result) {
-      return res.status(404).json({ message: 'Category not found' });
-    }
-    res.status(200).json({ message: 'Category deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-module.exports = {
-  getAllUsers,
-  createUser,
-  getAllCategories,
-  createCategory,
-  deleteUser,
-  deleteCategory,
 };
